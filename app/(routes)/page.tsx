a2a88b2d65e52440bd94f1fd9850b06e28d3672b
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-09-22 02:09:20 +0700

    My first commit. This is the commit for the Homepage present

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <div>Hello Store.</div>;
+};
+
+export default Homepage;
---
+++ components/navbar-action.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const NavbarAction = () => {
+  return (
+    <div>Hello Action.</div>
+  )
+}
+
+export default NavbarAction
