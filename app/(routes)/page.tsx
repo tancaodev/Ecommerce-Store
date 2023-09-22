c1b86518c9befb2d9a7e6480df3cf83cff0cc391
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-09-23 00:01:51 +0700

    Create billboard for homepage, add personal component like button. Create action to fetch data from URL

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const Homepage = () => {
-  return <div>Hello Store.</div>;
+import getBillboard from '@/actions/get-billboard';
+
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const Homepage = async () => {
+  const billboard = await getBillboard('50b9a2a3-33dd-47e4-9048-603c22cad340');
+
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default Homepage;
