689b847be00f85efb87197f8ea9535ef1de1d300
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2023-09-27 12:58:22 +0700

    Show product succesfully

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,17 +1,23 @@
 import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
 
 import Billboard from '@/components/billboard';
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
 const Homepage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard('50b9a2a3-33dd-47e4-9048-603c22cad340');
 
   return (
     <Container>
       <div className='space-y-10 pb-10'>
         <Billboard data={billboard} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title='Featured Products' items={products} />
+        </div>
       </div>
     </Container>
   );
